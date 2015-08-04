5c50a892a0a7a85498b96328e53b53e162929170
Guillaume Blanchet <gblanchet.list@gmail.com>
2015-08-04 15:33:39 -0400

    Remove hmscPoisson... for now

--- R/hmsc.R
+++ R/hmsc.R
@@ -6,11 +6,4 @@ function(data,param=NULL,priors=NULL,family="probit",niter=1000,nburn=500,thin=1
 		res<-hmscProbit(data,param=param,priors=priors,niter=niter,nburn=nburn,thin=thin,verbose=verbose)
 		return(res)
 	}
-
-	if(family=="poisson"){
-#		res<-hmscPoisson(data,param=param,priors=priors,niter=niter,nburn=nburn,thin=thin,verbose=verbose)
-		warning("I am currently fixing a bug here... be back soon !")	
-#		return(res)
-	}
-	
 }
