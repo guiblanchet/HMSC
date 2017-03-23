874d65c620ae4bb216369d1464864543adcec91e
Guillaume Blanchet <guiblanchet3@gmail.com>
2017-03-23 10:28:28 -0400

    Added sampleCondPred.cpp

---
+++ src/sampleCondPred.cpp
@@ -0,0 +1,32 @@
+#include <RcppArmadillo.h>
+#include <Rcpp.h>
+#include "sampleCondPred.h"
+
+using namespace arma;
+using namespace Rcpp;
+
+// Calculates a prediction conditional on a subset of species.
+arma::mat sampleCondPred(arma::mat& Y,
+				   arma::cube& EstModel,
+					 arma::vec residVar,
+					 int niter,
+					 double nsp,
+					 int nsite,
+				 	 std::string family) {
+
+	// Define object to store results
+	cube Ylatent(nsite, nsp, niter);
+	if(family == "probit"){
+		uvec Y0Loc = find(Y==0);
+		uvec Y1Loc = find(Y==1);
+
+		mat Ylatent = zeros(nsite,nsp);
+
+		for (int i = 0; i < niter; i++) {
+			Ylatent.slice(i) = sampleYlatentProbit(Y0Loc, Y1Loc, Ylatent, EstModel.slice(i), residVar, nsp, nsite)
+		}
+	}
+
+	// return result object
+	return Rcpp::Named("Ylatent", wrap(Ylatent));
+}
