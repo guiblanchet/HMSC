bf4f45b20159f4b2bfbd8d7308db27dbbd03ea35
Guillaume Blanchet <guiblanchet3@gmail.com>
2017-03-28 11:12:07 -0400

    Added sampleCondPred.h

---
+++ src/sampleCondPred.h
@@ -0,0 +1,16 @@
+#ifndef sampleCondPred_h
+#define sampleCondPred_h
+#include <RcppArmadillo.h>
+#include <Rcpp.h>
+#include "sampleYlatentProbit.h"
+
+RcppExport SEXP sampleCondPred(arma::mat& Y,
+				   arma::cube& EstModel,
+					 arma::mat residVar,
+					 int nsite,
+					 double nsp,
+					 int niter,
+					 int nsample,
+				 	 std::string family);
+
+#endif
