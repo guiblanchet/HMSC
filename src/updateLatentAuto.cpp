e1db2293493aa80636bcacc3a87328ec89531da1
Guillaume Blanchet <guiblanchet3@gmail.com>
2017-04-04 12:07:59 -0400

    Modified updateLatentAuto to speed up the analysis

--- src/updateLatentAuto.cpp
+++ src/updateLatentAuto.cpp
@@ -18,41 +18,41 @@ arma::field<arma::mat> updateLatentAuto(arma::mat& Yresid,
 										arma::vec& nLatentAuto,
 										double nsp,
 										int nsite){
-	
+
 	// Initiate a few basic object
 	int beginRow;
 	int beginCol;
 	int endRow;
 	int endCol;
-	
+
 	// Update latentAuto
 	for (int j = 0; j < nAuto; j++) {
-		
+
 		// Remove the effect of other autocorrelated latent variables, if any
 		for (int k = 0; k < nAuto; k++) {
 			if(j != k){
 				Yresid = Yresid - latentAuto(k,0).rows(RandomAuto.col(k))*trans(paramLatentAuto(k,0));
 			}
 		}
-		
+
 		// Define various objects
 		field<arma::mat> wAutoInvSel(1,nLatentAuto(j));
 		vec seqVec = regspace<vec>(1,nAutoLev(j));
-		
+
 		// Extract the cube of wAutoInv associated to the factor of Auto considered
 		cube wAutoInvFactor = wAutoInv(j,0);
-		
+
 		// Extract the matrix used to make the estimation in wAutoInv and convert it to a vector
 		for(int k = 0; k < nLatentAuto(j); k++){
-			// Find which paramAuto in priorParamAutoDistArma is the one used 
+			// Find which paramAuto in priorParamAutoDistArma is the one used
 			uvec paramAutoPointer = find(priorParamAutoDistArma.col(j)==as_scalar(paramAuto(j,0)(k)));
 			wAutoInvSel(0,k) = wAutoInvFactor.slice(paramAutoPointer(0));
 		}
 
-		// Construct a matrix of 0 
+		// Construct a matrix of 0
 		mat wAutoInvDiag = zeros(nLatentAuto(j)*nAutoLev(j),nLatentAuto(j)*nAutoLev(j));
 
-		
+
 		// Fill wAutoInvDiag on the diagonal with the matrix selected in wAutoInvSel
 		for(int k = 1; k <= nLatentAuto(j); k++){
 			beginRow = (k-1)*nAutoLev(j);
@@ -62,51 +62,63 @@ arma::field<arma::mat> updateLatentAuto(arma::mat& Yresid,
 
 			wAutoInvDiag.submat(beginRow,beginCol,endRow,endCol) = wAutoInvSel(0,k-1);
 		}
-		
+
 		// Construct a diagonal matrix with residVar
 		mat diagResidVar(nsp,nsp);
 		diagResidVar = diagmat(residVar);
-		
+
+		///////////////////////////////////////////////////////////////
 		// If the random effect is at the sampling unit level (faster!)
+		///////////////////////////////////////////////////////////////
 		if(nAutoLev(j)==nsite){
 			// Residual weighted paramLatentAuto;
 			mat wparamLatentAuto = diagResidVar*paramLatentAuto(j,0);
-			
+
 			// Precision matrix for the latentAuto variables
 			mat precLatent = wAutoInvDiag + kron(trans(paramLatentAuto(j,0))*wparamLatentAuto,eye(nsite,nsite));
-			
+
 			// Calculate the variance-covariance matrix for the latentAuto variables
-			mat varLatent = inv(precLatent);
-			
+//			mat varLatent = inv(precLatent);
+
 			// Calculate the means for the new latentAuto variables
-			mat meansLatent = varLatent*vectorise(Yresid*wparamLatentAuto); 
-			
+//			mat meansLatent = varLatent*vectorise(Yresid*wparamLatentAuto);
+
 			// Calculate the new latentAuto variables
-			latentAuto(j,0) = reshape(rmvnorm(1, meansLatent,varLatent),nAutoLev(j),nLatentAuto(j));
-			
+//			latentAuto(j,0) = reshape(rmvnorm(1, meansLatent,varLatent),nAutoLev(j),nLatentAuto(j));
+
+			// Calculate the cholesky decomposition of precLatent
+			mat precLatentChol = chol(precLatent,"lower");
+
+			// Calculate the means for the new latentAuto variables
+			mat meansLatent = solve(precLatent,vectorise(Yresid*wparamLatentAuto));
+
+			// Calculate the new latentAuto variables
+			latentAuto(j,0) = reshape(meansLatent + solve(precLatentChol,randn(nAutoLev(j)*nLatentAuto(j))), nAutoLev(j), nLatentAuto(j));
+		//////////////////////////////
 		// For any other random effect
+		//////////////////////////////
 		}else{
 			// Residual weighted paramLatentAuto;
 			mat wparamLatentAuto = diagResidVar*paramLatentAuto(j,0);
-			
+
 			// Construct a matrix defining how to weight each sample
 			mat diagMat = eye(nAutoLev(j),nAutoLev(j));
 			uvec lev = vectorise(RandomAuto.col(j));
 			mat diagMatLev = diagMat.cols(lev);
-			
+
 			// Precision matrix for the latentAuto variables
 			mat precLatent = wAutoInvDiag + kron(trans(paramLatentAuto(j,0))*wparamLatentAuto,diagMatLev*trans(diagMatLev));
-			
+
 			// Calculate the variance-covariance matrix for the latentAuto variables
 			mat varLatent = inv(precLatent);
-			
+
 			// Calculate the means for the new latentAuto variables
 			mat meansLatent = varLatent*vectorise(diagMatLev*Yresid*wparamLatentAuto);
-			
+
 			// Calculate the new latentAuto variables
 			latentAuto(j,0) = reshape(rmvnorm(1, meansLatent,varLatent),nAutoLev(j),nLatentAuto(j));
 		}
 	}
-	
+
 	return latentAuto;
 }
--- src/updateParamAuto.cpp
+++ src/updateParamAuto.cpp
@@ -14,46 +14,46 @@ arma::field<arma::vec> updateParamAuto(arma::field<arma::mat>& latentAuto,
 									   Rcpp::NumericMatrix& priorParamAutoDist,
 									   arma::vec& nLatentAuto,
 									   int nAuto){
-	
+
 	// Defining objects
 	double quadLike;
-	
+
 	for(int h = 0; h < nAuto; h++){
 		mat latentAutoMat = latentAuto(h,0);
 		cube wAutoInvCube = wAutoInv(h,0);
-		
+
 		vec paramAutoTmp(nLatentAuto(h));
 		for(int i = 0; i < nLatentAuto(h); i++){
 			// Construct an object to save likelihood results
 			Rcpp::NumericVector like(npriorParamAuto); // Rcpp code style
-		
+
 			// Build a few useful objects
 			mat latentAuto1 = latentAutoMat.col(i);
 			mat latentAuto1T = trans(latentAutoMat.col(i));
-			
+
 			for(int j = 0; j < npriorParamAuto; j++){
 				// Calculate the quadratic part of a multivariate normal log-likelihood that will be used to sample paramAuto
-				quadLike = as_scalar(latentAuto1T*wAutoInvCube.slice(j)*latentAuto1);
-		
+				quadLike = as_scalar(latentAuto1T*wAutoInvCube.slice(j)*latentAuto1); // This line of code is wrong and needs to be replaced. Check the line 34 of update_alpha.m
+
 				// Calculate the first part of the multivariate normal log-likelihood
 				like(j) = log(priorParamAutoWeight(j))-0.5*wAutoDet(j,h)-0.5*quadLike;
 			}
-			
+
 			// Recentre log-likelihood
 			like = like-max(like); // Rcpp code style
-			
+
 			// Convert log-likelihood to likelihood
 			like = exp(like);
-		
+
 			// Normalize the likelihood
 			like = like/sum(like);
-			
+
 			// Sample paramAuto
 			paramAutoTmp(i) = sampleNum(priorParamAutoDist,1,true,like)(0);
 		}
 		// Save object
 		paramAuto(h,0) = paramAutoTmp;
-		
+
 	}
 	// Return result
 	return paramAuto;
