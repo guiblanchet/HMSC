38401281397cd376d4d46f5965e7fc5eed576594
Guillaume Blanchet <guiblanchet3@gmail.com>
2017-06-07 08:24:21 -0400

    Some corrections to predict.hmsc and added an example on conditional prediction in the help file

--- src/sampleCondPred.cpp
+++ src/sampleCondPred.cpp
@@ -27,22 +27,15 @@ arma::cube sampleCondPred(arma::mat& Y,
 		mat YlatentIni = zeros(nsite,nsp);
 
 		for (int i = 0; i < nsample; i++) {
-//			for(int j = 0; j < niter; j++){
 					Ylatent.slice(i) = sampleYlatentProbit(Y0Loc, Y1Loc, YNALoc, YlatentIni, EstModel, residVar, nsp, nsite);
-//					YlatentTmp.slice(j) = sampleYlatentProbit(Y0Loc, Y1Loc, YNALoc, YlatentIni, EstModel.slice(j), trans(residVar.row(j)), nsp, nsite);
-//			}
-//			Ylatent(i,0) = YlatentTmp;
 		}
 	}
 
 	if(family == "gaussian"){
 		mat repResidVar(nsite,nsp);
 		for (int i = 0; i < nsample; i++) {
-//			for(int j = 0; j < niter; j++){
 				repResidVar = repmat(residVar,nsite,1);
 				Ylatent.slice(i) = randn(nsite,nsp)%repResidVar+EstModel;
-//			}
-//			Ylatent(i,0) = YlatentTmp;
 		}
 	}
 
@@ -50,10 +43,7 @@ arma::cube sampleCondPred(arma::mat& Y,
 		mat YlatentIni = zeros(nsite,nsp);
 
 		for (int i = 0; i < nsample; i++) {
-//			for(int j = 0; j < niter; j++){
 				Ylatent.slice(i) = sampleYlatentPoisson(Y, YlatentIni, EstModel, residVar, nsp, nsite);
-//			}
-//			Ylatent(i,0) = YlatentTmp;
 		}
 	}
 
